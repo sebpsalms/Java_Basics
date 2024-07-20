a7e1a01ebf0e41933b665a2ee20dc8d3b2273841
Sebastian <sebastianpsalms@gmail.com>
2024-07-20 22:49:55 +0530

    Example based on data-types

---
+++ dataTypes.java
@@ -0,0 +1,18 @@
+public class dataTypes {
+
+  public static void main(String args[])
+		{
+      int number2 =3; // variable declaration with variable type assigning
+      byte by =127; // only upto 127
+      
+      short sh =123;
+      long lo = 1234l; // need to give "l" at the end to represent long
+      float fl =2.5f; // need to give "f" at the end to represent float
+      double db = 2.4362572345;
+      char ch = 'k'; // need to give in single quotes
+      boolean b = true;
+
+			System.out.println( number2 + "," + by + "," + sh +"," + lo + "," + fl +"," + db + "," +  ch );
+		}
+  
+}
