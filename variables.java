2dad206bad87e027a7b2fb300037bd4c6bf3d817
Sebastian <sebastianpsalms@gmail.com>
2024-07-20 22:11:58 +0530

    Variables

---
+++ variables.java
@@ -0,0 +1,19 @@
+// The place where the data is stored temperally
+// Here java can be "Strongally typed language" that we can specifiy the type of the data need to be stored
+// Assign values from right hand side to left hand side
+
+public class variables {
+
+  
+	public static void main(String args[])
+		{
+      int number2 =3; // variable delaration with variable tyoe assigning
+      int number1 =5;
+      int sum = 0;
+      sum = number1 + number2;
+			System.out.println("= " + sum);
+		}
+}
+
+  
+
