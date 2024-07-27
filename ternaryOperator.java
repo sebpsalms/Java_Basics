55d2f6436e59748af59209019d559ddcee792b7b
Sebastian <sebastianpsalms@gmail.com>
2024-07-27 16:02:46 +0530

    Ternary operator example using ODD,EVEN

---
+++ ternaryOperator.java
@@ -0,0 +1,16 @@
+// Simple ternary operator example using odd even 
+
+class ternaryOperator
+{
+  public static void main(String[] args) 
+  {
+
+    int x = 3;
+    String result ;
+    
+    result = x%2==0 ? "even" : "odd" ;
+
+    System.out.println(result);
+    
+  }
+}
