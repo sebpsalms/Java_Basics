9c00690da50ef09d69c50fb126fee4c8f662e13f
Sebastian <sebastianpsalms@gmail.com>
2024-07-28 23:04:09 +0530

    Switch Statement examples using weeks

---
+++ switchStamt.java
@@ -0,0 +1,38 @@
+// Switch Statement examples using weeks
+
+public class switchStamt {
+
+  public static void main(String[] args) {
+    
+     int x = 5;
+
+     switch (x)
+     {
+      case (0):
+        System.out.println("Sunday");
+        break;
+      case (1):
+        System.out.println("Monday");
+        break;
+      case (2):
+        System.out.println("Tuesday");
+        break;
+      case (3):
+        System.out.println("Wednesday");
+        break;
+      case (4):
+        System.out.println("Thursdaay");
+        break;
+      case (5):
+        System.out.println("Friday");
+        break;
+      case (6):
+        System.out.println("Saturday");
+        break;
+      default :
+        System.out.println("Enter number in between 0-6");
+     }
+     }
+  }
+  
+
